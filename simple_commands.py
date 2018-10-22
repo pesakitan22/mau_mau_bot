84019dcc0785e6954f0688da880f241ee59e0a79
Deni Putra Perdana <deni.putra@bukalapak.com>
2018-10-22 19:00:09 +0700

    spacing;

--- simple_commands.py
+++ simple_commands.py
@@ -135,7 +135,7 @@ def stats(bot, update):
               n).format(number=n)
         )
         
-        n = us.first_places/us.games_played
+        n = us.first_places / us.games_played
         stats_text.append(
             _("{number} win percentage",
               "{number} win percentage",
